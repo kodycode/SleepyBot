005519ec994f3a566408424f195f34859fac4fa0
kodycode <kodythach@gmail.com>
2018-04-26 22:15:18 -0400

    Fixed token not being input

--- cogs/dbl.py
+++ cogs/dbl.py
@@ -28,4 +28,4 @@ class DiscordBotsOrgAPI:
 def setup(bot):
     with open('config.json') as config:
         config_data = json.load(config)
-    bot.add_cog(DiscordBotsOrgAPI(bot), config_data["dbl_token"])
+    bot.add_cog(DiscordBotsOrgAPI(bot, config_data["dbl_token"]))
